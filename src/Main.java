ad4bad87cb48fc360a9ebb16aecb30859e5463af
ibra <amerhanov_ibragim95@mail.ru>
2023-05-07 21:30:47 +0300

    exxx

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,9 @@ public class Main {
 
 
     public static void main(String[] args) {
+        String str = "Abc_123";
+        boolean result = str.matches("[a-zA-Z0-9_]+");
+        System.out.println(result);
         try {
             transport("java_skypro_go", "D_1hWiKjjP_9", "равен ConfilmPassword");
         } catch (WrongPasswordException e) {
@@ -12,6 +15,7 @@ public class Main {
         } catch (WrongLoginException e){
             throw new RuntimeException(e);
         }
+
         finally {
             System.out.println("финал-конец");
         }
