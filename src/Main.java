0085c7ef0c985fe63be888f79a0ecc2b2ddbd6ec
ibra <amerhanov_ibragim95@mail.ru>
2023-05-22 20:40:15 +0300

    exxx

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
         String str = "Abc_123";
         boolean result = str.matches("[a-zA-Z0-9_]+");
         System.out.println(result);
+// в логине не допустимые буквы и цифры
         try {
             transport("java_skypro_go", "D_1hWiKjjP_9", "равен ConfilmPassword");
         } catch (WrongPasswordException e) {
@@ -30,5 +31,7 @@ public class Main {
         if (password.length() >= 20 || !password.equals(confilmPassword)) throw new WrongPasswordException();
         //dsd
     }
+
+
 
     }
